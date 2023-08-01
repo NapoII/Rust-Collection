7743520adc5aced50dfc29c7a7c3c2b118fca6f7
NapoII <88168538+NapoII@users.noreply.github.com>
2023-08-01 20:16:09 +0200

    start

---
+++ README.md
@@ -0,0 +1,72 @@
+[![github/NapoII](https://raw.githubusercontent.com/NapoII/Rust-Collection
+/main/README_img/Readme_top.png)](https://github.com/NapoII)
+
+# Rust-Collection
+
+
+[![downloads/total](https://img.shields.io/github/downloads/NapoII/Rust-Collection
+/total)](https://github.com/NapoII/Rust-Collection
+/archive/refs/heads/main.zip) [![github/repo-size](https://img.shields.io/github/repo-size/NapoII/Rust-Collection
+)](https://github.com/NapoII/Rust-Collection
+/archive/refs/heads/main.zip) [![github/license](https://img.shields.io/github/license/NapoII/Rust-Collection
+)](https://github.com/NapoII/Rust-Collection
+/blob/main/LICENSE) [![github/last-commit](https://img.shields.io/github/downloads/NapoII/Rust-Collection
+/total)](https://img.shields.io/github/issues/NapoII/Rust-Collection
+?style=plastic) [![github/issues_open](https://img.shields.io/github/issues/NapoII/Rust-Collection
+?style=plastic)](https://img.shields.io/github/issues-raw/NapoII/Rust-Collection
+) [![github/stars](https://img.shields.io/github/stars/NapoII/Rust-Collection
+?style=social)](https://github.com/NapoII/Rust-Collection
+/stargazers) [![discord](https://img.shields.io/discord/190307701169979393)](https://discord.gg/knTKtKVfnr)
+
+This website is a comprehensive collection of links related to the game Rust. Here you will find a variety of useful resources, guides and community servers that can help you better understand the game and improve your skills.
+## 📝 Table of Contents
++ [Demo / Working](#demo)
++ [Install](#usage)
++ [How it works](#Use)
++ [Lizenz](#Lizenz)
+## 🎥 Demo / Working <a name = "demo"></a>
+coming soon...
+Rust-Collection
+## 💻 Install <a name = "usage"></a>
+```cmd
+git clone https://github.com/NapoII/Rust-Collection
+
+pip install -r requirements.txt
+```
+## 💭 How it works <a name = "Use"></a>
+
+start Rust-Collection
+.py directly from the folder or run in cmd:
+```cmd
+cd <local path of Rust-Collection
+>
+python Rust-Collection
+.py
+```
+
+## 📚 Lizenz <a name = "Lizenz"></a>
+MIT License
+
+Copyright (c) 2023 NapoII
+<small><small><small>
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+    
+<p align="center">
+<img src="https://raw.githubusercontent.com/NapoII/NapoII/233630a814f7979f575c7f764dbf1f4804b05332/Bottom.svg" alt="Github Stats" />
+</p>
---
+++ Rust_Collection/Wheel_of_Fortune_2.0/rotate.js
@@ -0,0 +1,271 @@
+let betPercentages = new Map();
+betPercentages.set('0', {
+  betColor: "5x (BLUE)",  
+  kellyBetPct: "0.023972872",
+  currentColor: "20x (RED)" 
+});
+betPercentages.set('1', {
+  betColor: "5x (BLUE)",
+  kellyBetPct: "0.024021592",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('2', {
+  betColor: "5x (BLUE)",
+  kellyBetPct: "0.015140644",
+  currentColor: "3x (Green)"
+});
+betPercentages.set('3', {
+  betColor: "10x (PURPLE)",
+  kellyBetPct: "0.010842996",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('4', {
+  betColor: "10x (PURPLE)",
+  kellyBetPct: "0.017316848",
+  currentColor: "5x (BLUE)"
+});
+betPercentages.set('5', {
+  betColor: "10x (PURPLE)",
+  kellyBetPct: "0.017370286",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('6', {
+  betColor: "10x (PURPLE)",
+  kellyBetPct: "0.009207527",
+  currentColor: "3x (Green)"
+});
+betPercentages.set('7', {
+  betColor: "5x (BLUE)",
+  kellyBetPct: "0.016999792",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('8', {
+  betColor: "5x (BLUE)",
+  kellyBetPct: "0.023926348",
+  currentColor: "10x (PURPLE)"
+});
+betPercentages.set('9', {
+  betColor: "5x (BLUE)",
+  kellyBetPct: "0.01513888",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('10', {
+  betColor: "NONE",
+  kellyBetPct: "0",
+  currentColor: "3x (Green)"
+});
+betPercentages.set('11', {
+  betColor: "NONE",
+  kellyBetPct: "0",
+  currentColor: "5x (BLUE)"
+});
+betPercentages.set('12', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.0059727175",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('13', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.0060228235",
+  currentColor: "5x (BLUE)"
+});
+betPercentages.set('14', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.00602548",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('15', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.0059895385",
+  currentColor: "3x (Green)"
+});
+betPercentages.set('16', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.006000217",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('17', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.0059564845",
+  currentColor: "10x (PURPLE)"
+});
+betPercentages.set('18', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.0059984845",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('19', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.006039634",
+  currentColor: "3x (Green)"
+});
+betPercentages.set('20', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.0059838685",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('21', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.0060115465",
+  currentColor: "5x (BLUE)"
+});
+betPercentages.set('22', {
+  betColor: "20x (RED)",
+  kellyBetPct: "0.0060124495",
+  currentColor: "1x (YELLOW)"
+});
+betPercentages.set('23', {
+  betColor: "NONE",
+  kellyBetPct: "0",
+  currentColor: "3x (Green)"
+});
+betPercentages.set('24', {
+  betColor: "5x (BLUE)",
+  kellyBetPct: "0.016948948",
+  currentColor: "1x (YELLOW)"
+});
+
+let betReturns = new Map();
+betReturns.set("NONE", {ret: 0});
+betReturns.set("1x (YELLOW)", {ret: 2});
+betReturns.set("3x (Green)", {ret: 4});
+betReturns.set("5x (BLUE)", {ret: 6});
+betReturns.set("10x (PURPLE)", {ret: 11});
+betReturns.set("20x (RED)", {ret: 21});
+
+
+(function() {
+  var R2D, active, angle, center, init, rotate, rotation, start, startAngle, stop;
+
+  active = false;
+
+  angle = 0;
+
+  rotation = 0;
+
+  startAngle = 0;
+
+  center = {
+    x: 0,
+    y: 0
+  };
+
+  document.ontouchmove = function(e) {
+    return e.preventDefault();
+  };
+
+  init = function() {
+    target.addEventListener("mousedown", start, false);
+    target.addEventListener("mousemove", rotate, false);
+    target.addEventListener("mouseup", stop, false);
+    
+    target.addEventListener("touchstart", start, false);
+    target.addEventListener("touchmove", rotate, false);
+    target.addEventListener("touchend", stop, false);
+  };
+  
+
+  R2D = 180 / Math.PI;
+
+  start = function(e) {
+    var height, left, top, width, x, y, _ref;
+    e.preventDefault();
+    _ref = this.getBoundingClientRect(), top = _ref.top, left = _ref.left, height = _ref.height, width = _ref.width;
+    center = {
+      x: left + (width / 2),
+      y: top + (height / 2)
+    };
+    x = e.clientX - center.x;
+    y = e.clientY - center.y;
+    startAngle = R2D * Math.atan2(y, x);
+    return active = true;
+  };
+
+  rotate = function(e) {
+    var d, x, y;
+    e.preventDefault();
+    x = e.clientX - center.x;
+    y = e.clientY - center.y;
+    d = R2D * Math.atan2(y, x);
+    rotation = d - startAngle;
+    updateGraphics();
+    if (active) {
+      return this.style.webkitTransform = "rotate(" + (angle + rotation) + "deg)";
+    }
+  };
+
+  stop = function() {
+    angle += rotation;
+    return active = false;
+  };
+
+  updateGraphics = function() {
+  var slot = (Math.floor(angle % 360 / (360 / 25)) + 25) % 25;
+    document.getElementById('currPos').innerHTML = slot;
+    document.getElementById('currCol').innerHTML = betPercentages.get("" + slot).currentColor;
+  };
+
+  init();
+
+}).call(this);
+
+function clearBets() {
+document.getElementById("betAmt").innerHTML = 0;
+  document.getElementById("betAdvice").innerHTML = "NONE";
+}
+
+
+function updateTable() {
+  var scrap = document.getElementById("currScrap").value;
+  var position = document.getElementById("currPos").innerHTML;
+  var positionField = document.getElementById("currCol").innerHTML;
+  var bet = document.getElementById("betAmt").innerHTML;
+  var prediction = document.getElementById("betAdvice").innerHTML;
+  
+  var table = document.querySelector("table");
+  var newRow = table.insertRow(-1);
+  var scrapCell = newRow.insertCell(0);
+  var positionCell = newRow.insertCell(1);
+  var positionFieldCell = newRow.insertCell(2);
+  var betCell = newRow.insertCell(3);
+  var predictionCell = newRow.insertCell(4);
+  
+  scrapCell.innerHTML = scrap;
+  positionCell.innerHTML = position;
+  positionFieldCell.innerHTML = positionField;
+  betCell.innerHTML = bet;
+  predictionCell.innerHTML = prediction;
+}
+
+
+
+function calcButtonClicked() {
+  var intvalue = document.getElementById('currPos').innerHTML;
+  var betPct = 0;
+  var betColor = "nothing";
+  var currentColor = "NONE";
+  if (betPercentages.has(intvalue)) {
+    console.log("Landed on " + intvalue);
+    betPct = betPercentages.get(intvalue).kellyBetPct;
+    betColor = betPercentages.get(intvalue).betColor;
+    currentColor = betPercentages.get(intvalue).currentColor;
+  } else {
+    console.error("Didn't land for " + intvalue);
+  }
+
+  var currentScrap = parseInt(document.getElementById('currScrap').value);
+  if (document.getElementById('doScrapCalc').checked) {
+    var lastAmtToBet = parseInt(document.getElementById('betAmt').innerHTML);
+    var lastBetColor = document.getElementById('betAdvice').innerHTML;
+    currentScrap -= lastAmtToBet;
+    if (currentColor == lastBetColor) {
+      currentScrap += lastAmtToBet * parseInt(betReturns.get(currentColor).ret); // TODO fix this as well
+    }
+  }
+  var currentScrap = document.getElementById('currScrap').value = currentScrap;
+
+  var amtToBet = Math.floor(betPct * currentScrap);
+  document.getElementById("betAmt").innerHTML = amtToBet;
+  document.getElementById("betAdvice").innerHTML = betColor;
+  updateTable();
+}
