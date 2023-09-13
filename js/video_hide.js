136a9ed66b43effea395c8cfba8ef96a2cb52275
NapoII <Napo_the_II@protonmail.com>
2023-09-14 00:31:07 +0200

    add vid to cads

---
+++ js/video_hide.js
@@ -0,0 +1,10 @@
+function showVideo() {
+    document.getElementById("video").style.display = "block";
+    document.querySelector(".background-image").style.display = "none";
+  }
+  
+  function hideVideo() {
+    document.getElementById("video").style.display = "none";
+    document.querySelector(".background-image").style.display = "block";
+  }
+  
