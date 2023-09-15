1d12449850eea64ef0fcdb5e56af4c42a09d2e43
NapoII <Napo_the_II@protonmail.com>
2023-09-15 21:50:47 +0200

    add stuff to discord emebd gen

--- discord/simpel_bot.py
+++ discord/simpel_bot.py
@@ -20,10 +20,18 @@ async def on_ready():
 
     # Erstelle ein Embed
     embed = discord.Embed(
-        title='Einfache Embed-Nachricht',
+        title='Hello on the Rust Collection Discord',
         description='Dies ist eine einfache Embed-Nachricht, die einmal gesendet wird.',
         color=discord.Color.blue()  # Farbe des Embeds
     )
+
+    # Set the image URL in the embed
+    image_url = "https://raw.githubusercontent.com/NapoII/Rust-Collection/main/discord/ideogram.jpeg"
+    embed.set_image(url=image_url)
+
+    # Add a field with a link
+    website_url = "https://napoii.github.io/Rust-Collection/"
+    embed.add_field(name="Website", value=f"[Visit Website]({website_url})")
 
     await channel.send(embed=embed)
 
