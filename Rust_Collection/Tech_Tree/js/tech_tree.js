6782dfe3a3dbc3703aac6c6c569189e9726569be
NapoII <Napo_the_II@protonmail.com>
2023-10-04 18:35:01 +0200

    bandarole of first card to doku

--- Rust_Collection/Tech_Tree/js/tech_tree.js
+++ Rust_Collection/Tech_Tree/js/tech_tree.js
@@ -1,41 +1,88 @@
-// Funktion zum Erstellen eines Nodes
-function createNode(id, x, y) {
-    const node = document.createElement("div");
-    node.className = "node";
-    node.style.left = x + "px";
-    node.style.top = y + "px";
-    node.innerText = "Node " + id;
-    document.getElementById("techtree").appendChild(node);
-    return node; // Rückgabewert, um den erstellten Node zu speichern
+const treeData = {
+    name: "Hammer",
+    children: [
+        {
+            name: "Axt",
+            children: [
+                {
+                    name: "Reifen"
+                }
+            ]
+        },
+        {
+            name: "Schaufel",
+            children: [
+                {
+                    name: "Sichel"
+                }
+            ]
+        }
+    ]
+};
+
+const margin = { top: 40, right: 120, bottom: 20, left: 120 };
+const width = 960 - margin.right - margin.left;
+const height = 500 - margin.top - margin.bottom;
+
+const svg = d3.select("#tree")
+    .append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+const treeLayout = d3.tree().size([width, height]);
+
+const root = d3.hierarchy(treeData);
+treeLayout(root);
+
+// Erstellen Sie Linien für die Verbindungen zwischen den Knoten
+const links = svg.selectAll(".link")
+    .data(root.links())
+    .enter().append("path")
+    .attr("class", "link")
+    .attr("d", d => {
+        return `M${d.source.x},${d.source.y}L${d.target.x},${d.target.y}`;
+    });
+
+// Erstellen Sie Buttons als Knoten
+const nodes = svg.selectAll(".node")
+    .data(root.descendants())
+    .enter().append("g")
+    .attr("class", "node")
+    .attr("transform", d => `translate(${d.x},${d.y})`);
+
+// Erstellen Sie Buttons als gleichgroße Vierecke
+nodes.append("rect")
+    .attr("x", -20)
+    .attr("y", -20)
+    .attr("width", 40)
+    .attr("height", 40)
+    .attr("class", "node-rect");
+
+// Fügen Sie Buttons hinzu
+nodes.append("foreignObject")
+    .attr("width", 40)
+    .attr("height", 40)
+    .html(d => `<button style="width:100%;height:100%;">${d.data.name}</button>`)
+    .on("click", function (d) {
+        // Ändern Sie die Farbe aller Verbindungen vom geklickten Button bis zum obersten Knoten
+        links.attr("stroke", link => {
+            const path = findPath(link.source, root);
+            return path.includes(d) ? "red" : "#000";
+        });
+    });
+
+function findPath(node, root) {
+    const path = [];
+    while (node !== root) {
+        path.push(node);
+        if (node.parent) {
+            node = node.parent;
+        } else {
+            break;
+        }
+    }
+    path.push(root);
+    return path;
 }
-
-// Funktion zum Erstellen einer Verbindung zwischen zwei Nodes
-function createConnection(node1, node2) {
-    const connection = document.createElement("div");
-    connection.className = "connection";
-    const rect1 = node1.getBoundingClientRect();
-    const rect2 = node2.getBoundingClientRect();
-    
-    const x1 = rect1.left + rect1.width / 2;
-    const y1 = rect1.top + rect1.height / 2;
-    const x2 = rect2.left + rect2.width / 2;
-    const y2 = rect2.top + rect2.height / 2;
-
-    const angle = Math.atan2(y2 - y1, x2 - x1);
-    const length = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-
-    connection.style.left = x1 + "px";
-    connection.style.top = y1 + "px";
-    connection.style.width = length + "px";
-    connection.style.transform = "rotate(" + angle + "rad)";
-    document.getElementById("techtree").appendChild(connection);
-}
-
-// Beispiel-Nodes erstellen und in Variablen speichern
-const node1 = createNode(1, 50, 50);
-const node2 = createNode(2, 200, 50);
-const node3 = createNode(3, 200, 200);
-
-// Beispiel-Verbindungen erstellen
-createConnection(node1, node2);
-createConnection(node1, node3);
