b37c69a586529174acda40012828862735fdf618
NapoII <Napo_the_II@protonmail.com>
2023-10-05 20:39:59 +0200

    fix c from skins card

--- js/calculateThursdayCountdown.js
+++ js/calculateThursdayCountdown.js
@@ -10,25 +10,14 @@ function updateCountdown() {
   const nextThursday = getNextThursday();
   const now = new Date();
 
-  if (nextThursday > now) {
-      const timeDifference = nextThursday - now;
-      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const timeDifference = Math.abs(nextThursday - now); // Verwende den Betrag der Differenz
 
-      const countdownElement = document.getElementById("ThursdayCountdown");
-      countdownElement.innerHTML = nextThursday.toDateString() + "<hr>" + days + " Days " + "<br>" + hours + " Hours " + "<br>" + minutes + " Minutes";
-  } else {
-      // Wenn der Donnerstag bereits in dieser Woche vorbei ist, berechne den Countdown für nächsten Donnerstag.
-      const nextThursday = getNextThursday();
-      const timeDifference = nextThursday - now;
-      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
 
-      const countdownElement = document.getElementById("ThursdayCountdown");
-      countdownElement.innerHTML = nextThursday.toDateString() + "<hr>" + days + " Days " + "<br>" + hours + " Hours " + "<br>" + minutes + " Minutes";
-  }
+  const countdownElement = document.getElementById("ThursdayCountdown");
+  countdownElement.innerHTML = nextThursday.toDateString() + "<hr>" + days + " Days " + "<br>" + hours + " Hours " + "<br>" + minutes + " Minutes";
 }
 
 // Aktualisiere den Countdown alle Sekunde
