af2d1da4c2ddf5f3644f5da013b94c39d835c09e
NapoII <Napo_the_II@protonmail.com>
2023-11-08 04:12:44 +0100

    change optic from input feald

--- py/discord/simpel_bot.py
+++ py/discord/simpel_bot.py
@@ -1,10 +1,10 @@
 import discord
 
 # Deine Discord-Bot-Token hier einfügen
-TOKEN = ""
+TOKEN = "MTEwMzQwNzU1MDkxOTA4NjE1MQ.GhVJnA.jbgHRDTT7qhCyHnI8ZjAGGjHQet5erv7RcXHR0"
 
 # Deine vorgegebene Channel-ID hier einfügen
-CHANNEL_ID =
+CHANNEL_ID = 1152263133109424159
 
 website_url = "https://napoii.github.io/Rust-Collection/"
 
@@ -23,22 +23,22 @@ async def on_ready():
     # Erstelle ein Embed
     embed = discord.Embed(
         title='Hello on the Rust Collection Discord',
-        description="""Hello on the Rust Collection Discord
-🌟 Explore Our New Feature: Upgrade Calc!
+        description="""🚧 Hello, Rust Innovators! 🌟
+🔧 Explore Our Newest Feature: Fertilizer Sales Estimator! 💰
 
-Greetings, Rust Enthusiasts!
+Greetings, Rust Fanatics!
 
-We are excited to introduce a fantastic addition to our Rust information website - the Starter Base Materials Calculator! Your feedback and suggestions have driven us to create this indispensable tool that will make your Rust gaming experience even more enjoyable.
+We're thrilled to unveil an exciting addition to our Rust gaming toolkit - the Fertilizer Sales Estimator! Your feedback and suggestions have inspired us to create this invaluable feature that will elevate your Rust gaming experience to the next level.
 
-🏡 What Does the Starter Base Materials Calculator Do?
-With this new tool, you can effortlessly and swiftly calculate the materials required for your starter base construction. No more guesswork or tedious manual calculations! Know exactly how much you need to gather before you embark on your building adventure.
-
+💼 What Can You Do with the Fertilizer Sales Estimator?
+With this latest update, you can now effortlessly and swiftly calculate your profits from selling fertilizers. No more guesswork or tedious manual calculations! Know precisely how much you're making when you cash in your fertilizers. Get ready to maximize your farming profits! 🌾💰🚜
+You can find it in the diesel calculator
 """,
         color=discord.Color.blue()  # Farbe des Embeds
     )
 
     # Set the image URL in the embed
-    image_url = "https://i.imgur.com/APz0sxZ.png"
+    image_url = "https://i.imgur.com/XSx2Kfy.png"
     embed.set_image(url=image_url)
 
     # Add a field with a link
