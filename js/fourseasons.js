fc6e90945e971cbd8b31339cac776755c5d6eeba
NapoII <Napo_the_II@protonmail.com>
2023-12-24 06:58:09 +0100

    the new Snow

--- js/fourseasons.js
+++ js/fourseasons.js
@@ -1,67 +1,82 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const canvas = document.createElement("canvas");
-  document.body.appendChild(canvas);
-  const ctx = canvas.getContext("2d");
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth(); // Der Monat ist nullbasiert (0-11)
 
-  canvas.style.position = "fixed";
-  canvas.style.pointerEvents = "none";
-  canvas.style.left = "0";
-  canvas.style.top = "0";
-  canvas.style.width = "100%";
-  canvas.style.height = "100%";
+  // Überprüfe, ob der aktuelle Monat Dezember oder Januar ist
+  if (currentMonth === 11 || currentMonth === 0) {
+    // Nur wenn es Dezember oder Januar ist, führe den Schneefall aus
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    const ctx = canvas.getContext("2d");
 
-  const snowflakes = [];
+    canvas.style.position = "fixed";
+    canvas.style.pointerEvents = "none";
+    canvas.style.left = "0";
+    canvas.style.bottom = "0"; // Platziere das Canvas am unteren Rand der Seite
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
 
-  function Snowflake() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.radius = Math.random() * 5 + 1;
-    this.speed = Math.random() * 3 + 1;
-    this.opacity = Math.random() * 0.5 + 0.1;
-
-    this.draw = function () {
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-      ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
-      ctx.fill();
-    };
-
-    this.update = function () {
-      this.y += this.speed;
-
-      if (this.y > canvas.height) {
-        this.y = 0;
-        this.x = Math.random() * canvas.width;
-      }
-
-      this.draw();
-    };
-  }
-
-  function createSnowflakes() {
-    for (let i = 0; i < 100; i++) {
-      snowflakes.push(new Snowflake());
-    }
-  }
-
-  function animateSnowflakes() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let i = 0; i < snowflakes.length; i++) {
-      snowflakes[i].update();
-    }
-
-    requestAnimationFrame(animateSnowflakes);
-  }
-
-  createSnowflakes();
-  animateSnowflakes();
-
-  window.addEventListener("resize", function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-  });
+
+    const snowflakes = [];
+
+    function Snowflake() {
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
+      this.radius = Math.random() * 5 + 1;
+      this.speed = Math.random() * 3 + 1;
+      this.opacity = Math.random() * 0.5 + 0.1;
+
+      this.draw = function () {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
+        ctx.fill();
+      };
+
+      this.update = function () {
+        this.y += this.speed;
+
+        // Wenn die Schneeflocke den unteren Rand erreicht hat,
+        // setze die Y-Position auf die Höhe des Canvas
+        if (this.y > canvas.height) {
+          this.y = 0;
+          this.x = Math.random() * canvas.width;
+        }
+
+        this.draw();
+      };
+    }
+
+    function createSnowflakes(count) {
+      for (let i = 0; i < count; i++) {
+        snowflakes.push(new Snowflake());
+      }
+    }
+
+    function animateSnowflakes() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      // Füge bei Bedarf neue Schneeflocken hinzu
+      if (Math.random() > 0.98) {
+        createSnowflakes(10); // Erhöhe die Anzahl der neuen Flocken
+      }
+
+      for (let i = 0; i < snowflakes.length; i++) {
+        snowflakes[i].update();
+      }
+
+      requestAnimationFrame(animateSnowflakes);
+    }
+
+    createSnowflakes(200); // Starte mit einer höheren Anzahl von Anfangsflocken
+    animateSnowflakes();
+
+    window.addEventListener("resize", function () {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    });
+  }
 });
